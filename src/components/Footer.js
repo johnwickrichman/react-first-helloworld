cc91b8a30e6f6beff18c8458778a2c75fea8572f
johnwickrichman <johnwickrichman@gmail.com>
2022-09-24 22:03:49 +0700

    add copyright + full year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
+      <hr />
         <h3>Footer</h3>
+        <h3>&copy; {new Date().getFullYear()}</h3>
     </div>
   )
 }

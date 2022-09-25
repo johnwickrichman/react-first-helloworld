6199ffbeea700de7c896d861e18606240eaf0eb9
johnwickrichman <johnwickrichman@gmail.com>
2022-09-25 17:44:21 +0700

    use custom hook in anather component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { logoStyle, titleStyle } from '../styles/style'
-
+import useHover from '../hooks/UseHover'
 
 const Logo = () => {
+
+  const [ hover, mouseOver, mouseOut ] = useHover()
 
     const logoImage = {
         url: './logo192.png'
@@ -11,7 +13,10 @@ const Logo = () => {
   return (
     <div>
         {/* <img src="./logo192.png" width="100" alt="logo" /> */}
-        <img src={logoImage.url} width="100" alt="logo" />
+        <img onMouseOver={mouseOver} onMouseOut={mouseOut} src={logoImage.url} width="100" alt="logo" />
+        {
+          hover ? <p>Hello Logo</p> : null
+        }
 
         <p style={logoStyle}>ทดสอบ style จาก style.js</p>
         <p style={titleStyle}>ทดสอบ style จาก style.js</p>

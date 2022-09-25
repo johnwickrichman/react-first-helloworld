f3e69e8994c1140232ea3faf300c507d0e25f5ac
johnwickrichman <johnwickrichman@gmail.com>
2022-09-25 16:41:15 +0700

    add toggle button with boolean useState

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,16 +4,21 @@ import React from 'react'
 const Sidebar = () => {
 
     // let fullname = "John"
-    const [fullname, setFullname] = React.useState('John')
+    const [fullname, setFullName] = React.useState('John')
+    const [isShow, setIsShow] = React.useState(true)
 
     const changeName = () => {
         // fullname = "Marry"
-        setFullname('Marry')
+        setFullName('Marry')
+        setIsShow(!isShow)
     }
 
   return (
     <>
     <p>Sidebar</p>
+    {
+        isShow ? <p>ค่า isShow เป็นจริง</p> : <p>ค่า isShow เป็นเท็จ</p>
+    }
 
     <p>สวัสดี {fullname}</p>
     <button onClick={changeName}>เปลี่ยนชื่อ</button>

45e5d18a0885300e880784381d337c8708f28463
johnwickrichman <johnwickrichman@gmail.com>
2022-09-26 18:55:06 +0700

    install react-router-dom and test in App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import NavBar from "./components/NavBar"
 import HomePage from "./page/HomePage"
 // import Button from 'react-bootstrap/Button';
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import AboutPage from "./page/AboutPage"
+
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <HomePage />
+      <Switch>
+
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+
+      </Switch>
       <Footer />
-    </>
+    </Router>
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import {Nav, Navbar,Container, NavDropdown   } from 'react-bootstrap'
+import React from "react"
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <>
+      <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+        <Container>
+          <NavLink className="navbar-brand" to="/" exact>
+            <img
+              alt=""
+              src="/logo192.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top me-3"
+            />{" "}
+            React Bootstrap
+          </NavLink>
 
-<Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-      <Container>
-      <Navbar.Brand href="#home">
+          {/* <Navbar.Brand href="#home">
             <img
               alt=""
               src="/logo192.png"
@@ -16,28 +27,39 @@ const NavBar = () => {
               className="d-inline-block align-top me-3"
             />{' '}
             React Bootstrap
-          </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">หน้าหลัก</Nav.Link>
-            <Nav.Link href="#link">เกี่ยวกับ</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Brand> */}
 
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              {/* <Nav.Link href="#home">หน้าหลัก</Nav.Link> */}
+              {/* <Nav.Link href="#link">เกี่ยวกับ</Nav.Link> */}
+
+              <NavLink to="/" className="nav-link" exact activeClassName="active">
+                หน้าหลัก
+              </NavLink>
+
+              <NavLink to="/about" className="nav-link" exact activeClassName="active">
+                เกี่ยวกับเรา
+              </NavLink>
+
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   )
 }
